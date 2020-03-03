7ed5eb8e825134c201f9f7c3715e487a01870fee
Andy Davies <1andydavies1@gmail.com>
2020-03-03 13:36:14 +0000

    Add npm and mongod to path

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,4 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  
-# https://stackoverflow.com/questions/46048453/aws-codedeploy-command-not-found
+export PATH=$PATH:/opt/bitnami/nodejs/bin/npm:/opt/bitnami/mongodb/bin/mongod
 
 cd ~/app
 mongod --fork --logpath /var/log/mongod.log
