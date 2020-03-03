2c63f8886b9b04387e2ea83024cc4d01242aef6d
Andy Davies <1andydavies1@gmail.com>
2020-03-03 14:21:49 +0000

    sudo

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,5 +2,5 @@ export PATH=$PATH:/opt/bitnami/nodejs/bin/:/opt/bitnami/mongodb/bin/
 
 cd ~/app
 mongod --fork --logpath /var/log/mongod.log
-npm install pm2 -g
+sudo npm install pm2 -g
 pm2 start src/index.js
