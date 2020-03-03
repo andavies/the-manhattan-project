0847e2bf9e983be4cf910a1326f798df6f1b1b40
Andy Davies <1andydavies1@gmail.com>
2020-03-03 13:20:05 +0000

    nvm

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,8 @@
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  
+# https://stackoverflow.com/questions/46048453/aws-codedeploy-command-not-found
+
 cd ~/app
 mongod --fork --logpath /var/log/mongod.log
 npm start
