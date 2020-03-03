71ba5c207ba094e8d3ce00e9714763af849c5915
Andy Davies <1andydavies1@gmail.com>
2020-03-03 16:28:32 +0000

    move npm install

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,7 @@
 export PATH=$PATH:/opt/bitnami/nodejs/bin/:/opt/bitnami/mongodb/bin/
 
 cd ~/app
+npm install
 mongod --fork --logpath /var/log/mongod.log
 sudo npm install forever -g
 forever start src/index.js
