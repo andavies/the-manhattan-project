16d00196efed59aa341a2a7bfca9b88aed30f48b
Andy Davies <Andy.Davies@unilever.com>
2020-03-04 13:17:38 +0000

    Fix mongo

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,6 +2,6 @@ export PATH=$PATH:/opt/bitnami/nodejs/bin/:/opt/bitnami/mongodb/bin/
 
 
 cd ~/app
-mongod --noauth --fork --logpath /var/log/mongod.log
+sudo mongod --noauth --fork --logpath /var/log/mongod.log --logappend --dbpath /opt/bitnami/mongodb/data/db
 sudo npm install forever -g
 forever start src/index.js
